2f11c2905b8b2b041079ca8acb1ac097960b986d
Justin-K-Ellis <jlke45@gmail.com>
2025-01-01 13:46:41 +0900

    Add PUT and DELETE operations for tag endpoint.

---
+++ controllers/tagController.js
@@ -0,0 +1,95 @@
+import { Router } from "express";
+import prisma from "../prisma/pClient.js";
+
+let tagRouter = Router();
+
+// GET
+// Get all
+tagRouter.get("/", async (req, res) => {
+  try {
+    let tags = await prisma.tag.findMany();
+    res.json(tags);
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a problem when fetching all tags.",
+      error: error,
+    });
+  }
+});
+
+// Get by id
+tagRouter.get("/:tagId", async (req, res) => {
+  let tagId = parseInt(req.params.tagId);
+  try {
+    let tag = await prisma.tag.findUnique({ where: { id: tagId } });
+    res.json(tag);
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error when fetching tag ${tagId}.`,
+      error: error,
+    });
+  }
+});
+
+// POST
+tagRouter.post("/", async (req, res) => {
+  let { name } = req.body;
+  try {
+    let newTag = await prisma.tag.create({
+      data: { name },
+    });
+    res.json(newTag);
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a problem when trying to create this tag.",
+      error: error,
+    });
+  }
+});
+
+// PUT
+tagRouter.put("/", async (req, res) => {
+  let { id, name } = req.body;
+  try {
+    let updatedTag = await prisma.tag.update({
+      where: { id },
+      data: { name },
+    });
+    res.json(updatedTag);
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error when updating tag ${id}.`,
+      error: error,
+    });
+  }
+});
+
+// DELETE
+// Delete by id
+tagRouter.delete("/:tagId", async (req, res) => {
+  let tagId = parseInt(req.params.tagId);
+  try {
+    let deleteTag = await prisma.tag.delete({ where: { id: tagId } });
+    res.json(deleteTag);
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error when deleting tag ${tagId}.`,
+      error: error,
+    });
+  }
+});
+
+// Delete all
+tagRouter.delete("/", async (req, res) => {
+  try {
+    let tags = await prisma.tag.deleteMany({});
+    res.json(tags);
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a problem when deleting all tags.",
+      error: error,
+    });
+  }
+});
+
+export default tagRouter;
--- routes.js
+++ routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRouter from "./controllers/userController.js";
 import postRouter from "./controllers/postController.js";
 import commentsController from "./controllers/commentsController.js";
+import tagRouter from "./controllers/tagController.js";
 
 let api = Router();
 
 api.use("/users", userRouter);
 api.use("/posts", postRouter);
 api.use("/comments", commentsController);
+api.use("/tags", tagRouter);
 
 export default api;
